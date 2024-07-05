b655bb004863506adb6395213831c1489a6bfb16
montaniaroja <adolfocp1972@gmail.com>
2024-07-04 22:56:25 -0600

    first commit

---
+++ backend/controllers/cuenta.js
@@ -0,0 +1,76 @@
+const { Cuenta, Transaccion } = require('../models');  // Asegúrate de que la ruta sea correcta
+
+module.exports = {
+    create(req, res) {
+        const { no_cuenta, id_cliente, moneda } = req.body;
+
+        if (!no_cuenta || !id_cliente || !moneda) {
+            return res.status(400).send({ message: 'Todos los campos son requeridos' });
+        }
+
+        return Cuenta.create({
+            no_cuenta,
+            id_cliente,
+            moneda
+        })
+        .then(cuenta => res.status(201).send(cuenta))
+        .catch(error => res.status(400).send({ message: error.message }));
+    },
+    list(_, res) {
+        return Cuenta.findAll({})
+            .then(cuentas => res.status(200).send(cuentas))
+            .catch(error => res.status(400).send({ message: error.message }));
+    },
+    find(req, res) {
+        const { id } = req.params;
+
+        return Cuenta.findOne({
+            where: { id }
+        })
+        .then(cuenta => {
+            if (!cuenta) {
+                return res.status(404).send({ message: 'Cuenta no encontrado' });
+            }
+            return res.status(200).send(cuenta);
+        })
+        .catch(error => res.status(400).send({ message: error.message }));
+    },
+    update(req, res) {
+        const { id } = req.params; // Obtenemos el id de los parámetros de la URL
+        const { saldo } = req.body;
+
+        if (!saldo) {
+            return res.status(400).send({ message: 'El saldo es requerido' });
+        }
+
+        return Cuenta.update({ saldo }, {
+            where: { id }
+        })
+        .then(rowsUpdated => {
+            if (rowsUpdated[0] === 0) {
+                return res.status(404).send({ message: 'Cuenta no encontrada' });
+            }
+            return res.status(200).send({ message: 'Cuenta actualizada correctamente' });
+        })
+        .catch(error => res.status(400).send({ message: error.message }));
+    },async findWithTransacciones(req, res){
+        const { id } = req.params;
+        
+        try{
+            const cuenta= await Cuenta.findOne({
+                where: { id },
+                include: {
+                    model: Transaccion,
+                    attributes:['fecha','tipo_movimiento','monto'],
+                },
+            });
+
+            if(!cuenta){
+                return res.status(404).send({message: 'Cuenta no encontrada'});
+            }
+            return res.status(200).send(cuenta);
+        }catch(error){
+            return res.status(400).send({ message: error.message });
+        }
+    }
+};
---
+++ backend/routes/index.js
@@ -0,0 +1,25 @@
+const clienteController = require('../controllers/cliente');
+const cuentaController = require('../controllers/cuenta');
+const transaccionController = require('../controllers/transaccion');
+
+module.exports = (app) => {
+    app.get('/api', (_req, res) => res.status(200).send({
+        message: 'Example project did not give you access to the API web services',
+    }));
+    app.post('/api/cliente/create', clienteController.create);
+    app.get('/api/cliente/list', clienteController.list);
+    app.get('/api/cliente/find/:id', clienteController.find);
+    app.get('/api/cliente/find/:id/cuentas', clienteController.findWithCuentas);  // esta ruta obtiene el cliente y sus
+    //cuentas asociadas
+    
+    app.post('/api/cuenta/create/:id_cliente', cuentaController.create);
+    app.get('/api/cuenta/list', cuentaController.list);
+    app.get('/api/cuenta/find/:id', cuentaController.find);
+    app.patch('/api/cuenta/:id', cuentaController.update);  // Usamos PATCH para actualizaciones parciales
+
+
+    app.post('/api/transaccion/create/', transaccionController.create);
+    app.get('/api/cuenta/find/:id/transacciones', cuentaController.findWithTransacciones);
+
+
+};
